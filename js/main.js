d8e56d2f887703c0658a019cddda43945a5d5f85
it1926 <janslivka55@seznam.cz>
2021-04-11 18:32:13 +0200

    New content progress

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+$(function(){
+     $("#obecne").on("click", function(){
+       jQuery("#politika h4, #politika p").toggle(2000);
+});});
+$(function(){
+    $("#statni").on("click", function(){
+      jQuery("#symboly h4, #symboly p,#symboly img").toggle(2000);
+});});
