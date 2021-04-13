a70685e636492438c5a771658515db9a49005089
it1926 <janslivka55@seznam.cz>
2021-04-13 19:30:16 +0200

    The final commit for now

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 
-$(function(){
-     $("#obecne").on("click", function(){
-       jQuery("#politika h4, #politika p").toggle(2000);
-});});
-$(function(){
-    $("#statni").on("click", function(){
-      jQuery("#symboly h4, #symboly p,#symboly img").toggle(2000);
-});});
+$(document).ready(function(){
+  
+    $("#obecne").on("click", function(){
+      $("#politika h4, #politika p").toggle(2000);
+    
+});
+
+   $("#statni").on("click", function(){
+     $("#symboly h4, #symboly p,#symboly img").toggle(2000);
+});
+
+
+});
+
+
+let nav = document.getElementById("sidebar");
+
+nav.addEventListener("click", function(){
+
+
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+});
