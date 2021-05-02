2035d24de2a65a7a4de9817fbd186d5dce5e8f56
it1926 <janslivka55@seznam.cz>
2021-05-02 16:36:19 +0200

    First progress of history

---
+++ js/historie.js
@@ -0,0 +1,80 @@
+
+const events = [
+    {
+       "stoleti":"15. století",
+       "udalost":"Sjednocení států Pyrenejského poloostrova do španělského království",
+       "podrobnosti":"V 15. století představoval maurské panství na Pyrenejském poloostrově už jen malý emirát Granada. Na druhé straně Kastilie položil sňatek dědičky kastilského trůnu Isabely Kastilské a dědice aragonského trůnu Ferdinanda II. Aragonského (1469) základy k trvalému sjednocení dvou nejmocnějších křesťanských států Pyrenejského poloostrova do španělského království. Formálně k němu došlo až nástupem na trůn jejich vnuka Karla I. Habsburského, neboť Isabela a Ferdinand vládli samostatně každý ve svém království a v zemi svého partnera byli pouze zástupci panovníka. ",
+       "url":"https://cs.wikipedia.org/wiki/%C5%A0pan%C4%9Blsko#15._stolet%C3%AD",
+       "evaluation":"true",
+       "img":"./img/15.jpg"
+    },
+    {
+        "stoleti":"16-17. století",
+        "udalost":"Španělsko za vlády Habsburků",
+        "podrobnosti":"V 16. století se Španělsko pod vládou španělských Habsburků Karla V. (1516–1556) a jeho syna Filipa II. (1556–1598) stalo nejmocnější zemí západní a střední Evropy. Období vlády Karla I. a Filipa II. se také označuje jako zlatý věk ve Španělsku. Mocenské postavení Filipa II. bylo založeno především na bohatství, které dovážely do země tzv. stříbrné konvoje z nově objevených zámořských zemí, a na rozsahu habsburských držav. Karel V. vládl nejen vlastnímu Španělsku, ale také neapolskému království a Sicílii, Nizozemí a obrovským zámořským državám. Právem mohl tvrdit, že nad jeho zemí Slunce nezapadá. Kromě toho byl dědicem tzv. dědičných habsburských zemí ve střední Evropě, které přenechal roku 1521–1522 bruselskými smlouvami mladšímu bratru Ferdinandovi. Roku 1519 byl zvolen římským králem a později korunován římským císařem (Karel V.). Roku 1580 byl Filip II. zvolen portugalským králem, a Portugalsko se tak spojilo personální unií se Španělskem.",
+        "url":"https://cs.wikipedia.org/wiki/%C5%A0pan%C4%9Blsko#15._stolet%C3%AD",
+        "evaluation":"false",
+        "img":"./img/16.png"
+    },
+    {
+       "stoleti":"18. století",
+       "udalost":"Války o dědictví",
+       "podrobnosti":"Roku 1700 zemřel poslední španělský král z rodu Habsburků Karel II., aniž by zanechal mužského dědice. Následně vypukly války o dědictví španělské (1701–1714), do nichž se zapojily všechny tehdejší evropské mocnosti s výjimkou Švédska, Ruska a Osmanské říše. O španělskou korunu usilovali především rakouští Habsburkové a francouzští Bourboni. Uspěli nakonec Bourboni, ovšem museli zaručit, že nikdy nedojde ke spojení francouzského a španělského trůnu v rukou jedné osoby. Španělsko též ztratilo své pozice ve Středomoří, neboť tamní území bylo postoupeno jakožto kompenzace rakouským Habsburkům a rodu savojských vévodů. Války prokázaly slabost Španělska, jehož území jimi bylo poničeno. Španělsko během nich přestalo být vnímáno jako velmoc.",
+       "url":"https://cs.wikipedia.org/wiki/%C5%A0pan%C4%9Blsko#15._stolet%C3%AD",
+       "evaluation":"true",
+       "img":"./img/16.jpg"
+    },
+    {
+        "stoleti":"19. století",
+        "udalost":"Invaze na Španělsko",
+        "podrobnosti":"Napoleon Bonaparte se rozhodl, že podnikne invazi do Španělska a na španělský trůn dosadil svého bratra Josepha. V letech 1808–1814 probíhal v zemi boj o nezávislost. V roce 1812 byla v Cádizu přijata první španělská ústava, na dobové poměry velmi liberální. Válka za nezávislost po Vídeňském kongresu vyústila v návrat Ferdinanda VII. na španělský trůn a obnovení absolutistických pořádků v zemi.",
+        "url":"https://cs.wikipedia.org/wiki/%C5%A0pan%C4%9Blsko#15._stolet%C3%AD",
+        "evaluation":"false",
+        "img":"./img/19.jpg"
+    },
+    {
+        "stoleti":"20. století",
+        "udalost":"Politická krize",
+        "podrobnosti":"Počátek 20. století byl ve Španělsku ve znamení klidu, míru[zdroj?] a ekonomického rozvoje. Politická krize vedla však nejprve k nastolení diktatury Miguela Primo de Rivery, a poté k vyhlášení druhé španělské republiky a odchodu španělského krále Alfonse XIII. do exilu. Sílící sociální a politické konflikty vedly k vypuknutí občanské války, která si vyžádala přes půl milionu obětí.[2] V roce 1939 válka skončila nástupem diktátora Francisca Franca, který vládl až do své smrti v roce 1975. Odhaduje se, že během občanské války a po válce nechal Franco popravit na 180 000 lidí.[3] Již za své vlády Franco rozhodl, že Španělsko se stane monarchií a připravil předání moci vnukovi svrženého krále Juanu Carlosovi.",
+        "url":"https://cs.wikipedia.org/wiki/%C5%A0pan%C4%9Blsko#15._stolet%C3%AD",
+        "evaluation":"true",
+        "img":"./img/20.jpg"
+    },
+    {
+        "stoleti":"21. století",
+        "udalost":"Současnost",
+        "podrobnosti":"Od 1. ledna 2002 zavedla země euro, které nahradilo předchozí španělskou pesetu. Španělská ekonomika v té době rychle rostla zejména následkem stavebního boomu; to však skončilo s ekonomickou krizí roku 2008. Počátek 21. století je zároveň dobou velmi silné imigrace obyvatel zejména z Jižní Ameriky, Maroka a jihovýchodní Evropy.Dne 11. března 2004 se madridské příměstské vlaky staly terčem teroristických útoků organizace al-Káida. Při útocích zemřelo 192 lidí a 1460 lidí bylo zraněno. Tyto útoky a neobratná snaha španělské vlády připsat je ETA jsou považovány za jeden z důvodů nečekaného vítězství socialistů ve volbách (42,59 % hlasů) nad favorizovanými lidovci Josého Aznara. Dalším důvodem byl liknavý postup při řešení ekologické katastrofy, kterou způsobila havárie ropného tankeru Prestige u břehů Galicie v listopadu 2002, a Aznarova podpora války v Iráku.",
+        "url":"https://cs.wikipedia.org/wiki/%C5%A0pan%C4%9Blsko#15._stolet%C3%AD",
+        "evaluation":"false",
+        "img":"./img/15.jpg"
+    },
+ ];
+
+ $(document).ready(function(){
+  
+    events.forEach((event)=>{
+        /* Metoda append() přidává nové prvky do vybrané částí stránky (vždy za už existující obsah) */
+        $("#udalosti tbody").append(`<tr>
+            <td class="event-year">${event.stoleti}</td>
+            <td>
+              <p class="event-name"><i class="fas fa-chevron-down"></i> <a href="${event.url}" target="_new">${event.event}</a></p>
+              <p class="event-detail">${event.detail}</p>
+            </td>            
+            <td class="event-evaluation">${(event.evaluation == "true") ? '<i class="fas fa-plus-circle text-success"></i>' : '<i class="fas fa-minus-circle text-danger"></i>'}</td>
+        </tr>`);
+    });
+
+
+
+    
+    $("#Obecne").on("click", function(){
+      $("#Pocatek h4, #Pocatek p, #Pocatek img").toggle(2000);
+    
+});
+
+   $("#statni").on("click", function(){
+     $("#symboly h4, #symboly p,#symboly img").toggle(2000);
+});
+
+
+});
